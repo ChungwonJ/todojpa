4e9e158ff86bf78f11928f3ce86a5c47f315b3cb
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-02-06 22:47:24 +0900

    signin error 401

--- src/main/java/com/example/todojpa/service/UserService.java
+++ src/main/java/com/example/todojpa/service/UserService.java
@@ -5,7 +5,9 @@ import com.example.todojpa.dto.user.res.SignUpResponseDto;
 import com.example.todojpa.entity.User;
 import com.example.todojpa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -22,9 +24,15 @@ public class UserService {
 
     public SignInResponseDto login(String email, String password) {
         User user = userRepository.findByEmail(email);
-        if (user != null && user.getPassword().equals(password)) { // 평문 비교
-            return new SignInResponseDto(user.getId(), user.getUsername());
+
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid email or password");
         }
-        throw new RuntimeException("Invalid email or password");
+
+        if (user.getPassword().equals(password)) {
+            return new SignInResponseDto(user.getId(), user.getUsername());
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Password incorrect");
+        }
     }
 }
